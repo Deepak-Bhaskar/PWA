adb28a70284f79b3f49fe352625bfaf4bffb6bd6
Deepak <deepak27090@gmail.com>
2021-01-02 17:40:28 +0530

    Polyfile added for promise and fetch.

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,8 @@
+//Checking promises for legacy browser
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
 // Checking service worker is available for browser or not.
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("./sw.js").then(function () {
