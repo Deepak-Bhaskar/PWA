56ce144a44f4a0a98870f2be25d7d616eee99e0f
Deepak <deepak27090@gmail.com>
2021-01-03 15:41:12 +0530

    indexDB setup and imntegrate CRUD operation

--- public/sw.js
+++ public/sw.js
@@ -1,81 +1,7 @@
-// var CACHE_STATIC_NAME = "static";
-// var CACHE_DYNAMIC_NAME = "dynamic";
+importScripts("/src/js/idb.js");
+importScripts("/src/js/utility.js");
 
-// // Installing service worker
-// // Self is pointing to service worker here.
-// // Lifecycle event is triggered by your SW
-// self.addEventListener("install", function (event) {
-//   console.log("[Service Worker] Installing Service Worker ...", event);
-//   event.waitUntil(
-//     caches.open(CACHE_STATIC_NAME).then(function (cache) {
-//       console.log("[Service Worker] Precaching App Shell");
-//       cache.addAll([
-//         "/",
-//         "/index.html",
-//         "/offline.html",
-//         "/src/js/app.js",
-//         "/src/js/feed.js",
-//         "/src/js/promise.js",
-//         "/src/js/fetch.js",
-//         "/src/js/material.min.js",
-//         "/src/css/app.css",
-//         "/src/css/feed.css",
-//         "/src/images/main-image.jpg",
-//         "https://fonts.googleapis.com/css?family=Roboto:400,700",
-//         "https://fonts.googleapis.com/icon?family=Material+Icons",
-//         "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
-//       ]);
-//     })
-//   );
-// });
-
-// // Activating service worker
-// self.addEventListener("activate", function (event) {
-//   console.log("[Service Worker] Activating service worker...!!!", event);
-//   // Returning claims() method to ensure our SW loaded correctly, it will work without it but writing return ensures the SW loaded correctly.
-//   event.waitUntil(
-//     caches.keys().then(function (keyList) {
-//       return Promise.all(
-//         keyList.map(function (key) {
-//           if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-//             console.log("[Service Worker] Removing old cache.", key);
-//             return caches.delete(key);
-//           }
-//         })
-//       );
-//     })
-//   );
-//   return self.clients.claim();
-// });
-
-// // One more thing fetch is triggered by your webpage
-// // Now adding non lifecycle event
-// self.addEventListener("fetch", function (event) {
-//   // now suupose we want to overwrite response coming from fetch request we can do it by respondWith
-//   // event.respondWith(null);  This site can't be eached
-//   event.respondWith(
-//     caches.match(event.request).then(function (response) {
-//       if (response) {
-//         return response;
-//       } else {
-//         return fetch(event.request)
-//           .then(function (res) {
-//             return caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
-//               cache.put(event.request.url, res.clone());
-//               return res;
-//             });
-//           })
-//           .catch(function (err) {
-//             return caches.open(CACHE_STATIC_NAME).then(function (cache) {
-//               return cache.match("/offline.html");
-//             });
-//           });
-//       }
-//     })
-//   ); // respond with event request
-// });
-
-var CACHE_STATIC_NAME = "static-v15";
+var CACHE_STATIC_NAME = "static-v18";
 var CACHE_DYNAMIC_NAME = "dynamic-v2";
 var STATIC_FILES = [
   "/",
@@ -83,6 +9,7 @@ var STATIC_FILES = [
   "/offline.html",
   "/src/js/app.js",
   "/src/js/feed.js",
+  "/src/js/idb.js",
   "/src/js/promise.js",
   "/src/js/fetch.js",
   "/src/js/material.min.js",
@@ -147,15 +74,21 @@ function isInArray(string, array) {
 }
 
 self.addEventListener("fetch", function (event) {
-  var url = "https://httpbin.org/get";
+  var url = "https://pwa-project-e90ee-default-rtdb.firebaseio.com/posts";
   if (event.request.url.indexOf(url) > -1) {
     event.respondWith(
-      caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
-        return fetch(event.request).then(function (res) {
-          // trimCache(CACHE_DYNAMIC_NAME, 3);
-          cache.put(event.request, res.clone());
-          return res;
-        });
+      fetch(event.request).then(function (res) {
+        var clonedRes = res.clone();
+        clearAllData("posts")
+          .then(function () {
+            return clonedRes.json();
+          })
+          .then(function (data) {
+            for (var key in data) {
+              writeData("posts", data[key]);
+            }
+          });
+        return res;
       })
     );
   } else if (isInArray(event.request.url, STATIC_FILES)) {
